a5efe962ae3bc01ecc6ca6e3609ce43d9140b2e1
haven <haven.eldebaque@gmail.com>
2023-07-18 09:32:17 +1000

    initial style pushed to Making_basic_routes

--- app/auth.py
+++ app/auth.py
@@ -3,3 +3,17 @@ from flask import Blueprint
 # we have set a Blueprint for our flask app
 auth = Blueprint('auth', __name__)
 
+
+@auth.route('/login')
+def login():
+    return "<p> My Login Page </p>"
+
+
+@auth.route('/logout')
+def logout():
+    return "<p> You are logged out  </p>"
+
+
+@auth.route('/signup')
+def sign_up():
+    return "<p> sign-up  </p>"
